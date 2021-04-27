121311240c8da7d32daff2e882d266b9ca424018
Wade Wright <wwright2@gmail.com>
2021-04-27 12:49:48 -0400

    feat(network-host): Use network host

--- src/command.js
+++ src/command.js
@@ -103,7 +103,7 @@ const command = {
    * @returns {array} Link arguments
    */
   getLinks: (cfg) => _.chain(_.pipe([_.toPairs, _.head, ([key, value]) => {
-    return [cfg.networkHost === true ? '' : '--link', `${command.getName(key, value)}:${key}`]
+    return [cfg.networkHost === true ? undefined : '--link', `${command.getName(key, value)}:${key}`].filter(item => Boolean(item))
   }]))(cfg.services || []),
   /**
    * Returns full command arguments array
